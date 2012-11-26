c0835679ded8ab716f413fb91bbbdc5b83aa7340
Jason Thompson <jason@jthompson.ca>
2012-11-26 06:02:42 -0500

    added simple sin-osc synth melody

--- src/sleave/songs/evaels.clj
+++ src/sleave/songs/evaels.clj
@@ -52,7 +52,7 @@
   (at (m (+ 4 beat-num))(play-chord a-synth (chord :G3 :major)))
   (at (m (+ 8 beat-num))(play-chord a-synth (chord :A3 :minor)))
   (at (m (+ 12 beat-num))(play-chord  a-synth(chord :G3 :major)))
-  (apply-at (m (+ 16 beat-num)) shudder2-chord-progression m (+ 16 beat-num) a-synth []))
+  (apply-at (m (+ 16 beat-num)) chord-progression m (+ 16 beat-num) a-synth []))
 
 (chord-progression metro (metro) shudder2)
 
@@ -66,9 +66,9 @@
 
 (easy 440)
 
-(def partials [1.1 1.2 1.4])
+(def partials [0.99 1.1 1.4])
 
-(defsynth easy2 [freq 440 vibrato 10 attack 0.4 sustain 0.4 release 4 ]
+(defsynth easy2 [freq 440 vibrato 10 attack 0.4 sustain 0.4 release 2]
   (let [lfo (sin-osc:kr vibrato)
         osc1 (sin-osc freq)
         osc2 (sin-osc (* freq (nth partials 0)))
@@ -80,7 +80,38 @@
         env4 (env-gen (lin-env attack (* sustain 1.1) release) 1 1 0 1 FREE)]
     (out 0 (pan2 (mix [(* osc1 env1) (* osc2 env2) (* osc3 env3) (* osc4 env4)])))))
 
-(easy2)
+(defsynth blippish [freq 440 attack 0.01 release 1 level 1 curve -4]
+  (let [env (env-gen (perc attack release level curve) :action FREE)
+        snd (sin-osc freq)
+        snd (distort snd)]
+    (out 0 (pan2 (* env snd)))))
+
+(blippish 440)
+(easy2 440)
 (stop)
 
-;; Effects
+(def metro (metronome 100))
+
+(metro)
+
+(defn play [m beat-num]
+  (at (m (+ beat-num 0)) (easy2 (midi->hz (note "C3"))))
+  (at (m (+ beat-num 4)) (easy2 (midi->hz (note "C3"))))
+  (at (m (+ beat-num 8)) (easy2 (midi->hz (note "C3"))))
+  (at (m (+ beat-num 12)) (easy2 (midi->hz (note "C3"))))
+  (apply-at (m (+ beat-num 16)) play metro (+ 16 beat-num) []))
+
+(play metro (metro))
+
+(defn play-blippish [m beat-num]
+  (at (m (+ beat-num 0)) (blippish (midi->hz (note "C4"))))
+  (at (m (+ beat-num 0.5)) (blippish (midi->hz (note "G3"))))
+  (at (m (+ beat-num 1)) (blippish (midi->hz (note "B3"))))
+  (at (m (+ beat-num 1.5)) (blippish (midi->hz (note "F3"))))
+  (at (m (+ beat-num 3)) (blippish (midi->hz (note "B4"))))
+  (apply-at (m (+ beat-num 8)) play-blippish metro (+ beat-num 8) []))
+
+(play metro (metro))
+(play-blippish metro (metro))
+
+(stop)
